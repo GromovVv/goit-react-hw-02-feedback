209d2bde70b60467c5e42e55328dd83d14a1429c
GromovVv <pavel.kuts@gmail.com>
2021-04-27 03:42:06 +0300

    ready

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,21 +1,31 @@
 import PropTypes from 'prop-types';
 
-import Notifications from '../Notifications'
-import './Statistics.scss'
+import Notifications from '../Notifications';
+import './Statistics.scss';
+
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+// import CircularProgressbar from '../StatusBar';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div>
       {good > 0 || neutral > 0 || bad > 0 ? (
-        <ul className="stats__list">
-          <li className="stats__item">Good: {good}</li>
-          <li className="stats__item">Neutral: {neutral}</li>
-          <li className="stats__item">Bad: {bad}</li>
-          <li className="stats__item">Total: {total}</li>
-          <li className="stats__item">
-            Positive feedback: {positivePercentage}%
-          </li>
-        </ul>
+        <div className="stats__wrapper">
+          <ul className="stats__list">
+            <li className="stats__item">Good: {good}</li>
+            <li className="stats__item">Neutral: {neutral}</li>
+            <li className="stats__item">Bad: {bad}</li>
+            <li className="stats__item">Total: {total}</li>
+            <li className="stats__item">
+              Positive feedback: {positivePercentage}%
+            </li>
+          </ul>
+          <div className="stats__progressBar">
+            <CircularProgressbar value={positivePercentage} text={`${positivePercentage}%`} />
+          </div>
+        </div>
       ) : (
         <Notifications message="No feedback given"></Notifications>
       )}
